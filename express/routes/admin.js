1783fb9919acaa2e3bbba8aed268c238a2c28d88
unknown <majdi.mokhtar@gmail.com>
2022-11-22 22:44:51 +0100

    node express html and routing

--- express/routes/admin.js
+++ express/routes/admin.js
@@ -1,13 +1,15 @@
+const path = require("path")
+
 const express = require("express")
+
+
+const rootDir = require("../util/path")
 
 const router = express.Router()
 
 // get request admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-  console.log("in the middleware 2")
-  res.send(
-    "<form action='/admin/add-product' method='POST' ><input type='text' name='title'/><button type='submit'>Add Product</button></form>"
-  )
+  res.sendFile(path.join(__dirname, rootDir, "views", "add-product.html"))
 })
 // post request admin/add-product =>POST
 router.post("/add-product", (req, res) => {
--- express/routes/shop.js
+++ express/routes/shop.js
@@ -1,13 +1,13 @@
+const path = require("path")
+
 const express = require("express")
+
+const rootDir = require("../util/path")
 
 const router = express.Router()
 
-
-
 router.get("/", (req, res, next) => {
-    console.log("in the middleware 2")
-    res.send("<h1>Hello from express</h1>")
-  })
+  res.sendFile(path.join(__dirname, rootDir, "views", "shop.html"))
+})
 
-
-  module.exports = router
+module.exports = router
