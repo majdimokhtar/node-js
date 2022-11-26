20e3d7271aa1f777edaf7923d8b37e900e137dea
unknown <majdi.mokhtar@gmail.com>
2022-11-26 20:04:19 +0100

    fix

--- dynamic-routing/models/cart.js
+++ dynamic-routing/models/cart.js
@@ -41,16 +41,27 @@ module.exports = class Cart {
       if (err) {
         return
       }
-      const updatedCart = { ...fileContent }
+      const updatedCart = { ...JSON.parse(fileContent) }
       const product = updatedCart.products.find((prod) => prod.id === id)
       const productQty = product.qty
       updatedCart.products = updatedCart.products.fillter(
         (prod) => prod.id !== id
       )
-      updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty
+      updatedCart.totalPrice =
+        updatedCart.totalPrice - productPrice * productQty
       fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
         console.log(err)
       })
+    })
+  }
+  static getCart(cb) {
+    fs.readFile(p, (err, fileContent) => {
+      const cart = JSON.parse(fileContent)
+      if (err) {
+        cb(null)
+      } else {
+        cb(cart)
+      }
     })
   }
 }
