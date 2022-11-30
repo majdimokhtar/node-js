c08da894658760de0e2ac07e606aa4dce5219675
unknown <majdi.mokhtar@gmail.com>
2022-11-30 20:08:28 +0100

    sequelize crud

--- sequelize/app.js
+++ sequelize/app.js
@@ -7,6 +7,8 @@ const errorController = require("./controllers/error")
 const sequelize = require("./util/database")
 const Product = require("./models/product")
 const User = require("./models/user")
+const Cart = require("./models/cart")
+const CartItem = require("./models/cart-item")
 
 const app = express()
 
@@ -34,6 +36,10 @@ app.use(errorController.get404)
 
 Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" })
 User.hasMany(Product)
+User.hasOne(Cart)
+Cart.belongsTo(User)
+Cart.belongsToMany(Product, { through: CartItem })
+Product.belongsToMany(Cart, { through: CartItem })
 
 sequelize
   // .sync({force : true})
@@ -49,7 +55,11 @@ sequelize
     return user
   })
   .then((user) => {
-    app.listen(3000, console.log("server is running on port 3000"))
+    return user.createCart()
+
     // console.log(user)
+  })
+  .then((cart) => {
+    app.listen(3000, console.log("server is running on port 3000"))
   })
   .catch((err) => console.log(err))
