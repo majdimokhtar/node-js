2d470e296053c4613214e2d63cd59ea57334d77b
unknown <majdi.mokhtar@gmail.com>
2022-12-06 15:30:01 +0100

    crud with mongoose

--- Mongoose/controllers/admin.js
+++ Mongoose/controllers/admin.js
@@ -60,15 +60,15 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl
   const updatedDesc = req.body.description
 
-  const product = new Product(
-    updatedTitle,
-    updatedPrice,
-    updatedDesc,
-    updatedImageUrl,
-    prodId
-  )
-  product
-    .save()
+  Product.findById(prodId)
+    .then((product) => {
+      product.title = updatedTitle
+      product.price = updatedPrice
+      product.imageUrl = updatedImageUrl
+      product.description = updatedDesc
+      return product.save()
+    })
+
     .then((result) => {
       console.log("UPDATED PRODUCT!")
       res.redirect("/admin/products")
@@ -77,7 +77,7 @@ exports.postEditProduct = (req, res, next) => {
 }
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then((products) => {
       res.render("admin/products", {
         prods: products,
@@ -90,7 +90,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId
-  Product.deleteById(prodId)
+  Product.findByIdAndRemove(prodId)
     .then(() => {
       console.log("DESTROYED PRODUCT")
       res.redirect("/admin/products")
