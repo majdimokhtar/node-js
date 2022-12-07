7d87c6631fbf97dc63be8fda8ef7a7edac4ff357
unknown <majdi.mokhtar@gmail.com>
2022-12-07 22:26:46 +0100

    continue working with post orders

--- Mongoose/controllers/shop.js
+++ Mongoose/controllers/shop.js
@@ -86,7 +86,7 @@ exports.postOrder = (req, res, next) => {
     // .execPopulate()
     .then((user) => {
       const products = user.cart.items.map((i) => {
-        return { quantity: i.quantity, product: i.productId }
+        return { quantity: i.quantity, product: { ...i.productId._doc } }
       })
       const order = new Order({
         user: {
@@ -98,14 +98,16 @@ exports.postOrder = (req, res, next) => {
       return order.save()
     })
     .then((result) => {
+      req.user.clearCart()
+    })
+    .then(() => {
       res.redirect("/orders")
     })
     .catch((err) => console.log(err))
 }
 
 exports.getOrders = (req, res, next) => {
-  req.user
-    .getOrders()
+  Order.find({ "user.userId": req.user._id })
     .then((orders) => {
       res.render("shop/orders", {
         path: "/orders",
