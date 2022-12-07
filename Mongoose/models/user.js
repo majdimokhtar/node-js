ff0a03d4ed95cf493d22065ccb31f8c010a2cc68
unknown <majdi.mokhtar@gmail.com>
2022-12-07 19:48:20 +0100

    add order functionality

--- Mongoose/models/user.js
+++ Mongoose/models/user.js
@@ -44,11 +44,17 @@ userSchema.methods.addToCart = function (product) {
   const updatedCart = {
     items: updatedCartItems,
   }
-  this.cart =updatedCart
+  this.cart = updatedCart
   return this.save()
 }
 
-
+userSchema.methods.removeFromCart = function (productId) {
+  const updatedCartItems = this.cart.items.filter((item) => {
+    return item.productId.toString() !== productId.toString()
+  })
+  this.cart.items = updatedCartItems
+  return this.save()
+}
 
 module.exports = mongoose.model("User", userSchema)
 
