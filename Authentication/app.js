ae7d97a8e32ef3c6b57cd6205c7cb9f3c98f343b
unknown <majdi.mokhtar@gmail.com>
2022-12-09 10:24:03 +0100

    node js auth

---
+++ Authentication/app.js
@@ -0,0 +1,79 @@
+const path = require("path")
+
+const express = require("express")
+require("dotenv").config()
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
+const session = require("express-session")
+const MongoDBStore = require("connect-mongodb-session")(session)
+
+const errorController = require("./controllers/error")
+const User = require("./models/user")
+
+const MONGODB_URI = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_cluster}.x5apesq.mongodb.net/${process.env.mongodb_database_key}?retryWrites=true&w=majority`
+
+const app = express()
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: "sessions",
+})
+
+app.set("view engine", "ejs")
+app.set("views", "views")
+
+const adminRoutes = require("./routes/admin")
+const shopRoutes = require("./routes/shop")
+const authRoutes = require("./routes/auth")
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, "public")))
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+)
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next()
+  }
+  User.findById(req.session.user._id)
+    .then((user) => {
+      req.user = user
+      next()
+    })
+    .catch((err) => console.log(err))
+})
+
+app.use("/admin", adminRoutes)
+app.use(shopRoutes)
+app.use(authRoutes)
+
+app.use(errorController.get404)
+
+mongoose
+  .connect(MONGODB_URI)
+  .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "Majdi",
+          email: "majdi@test.com",
+          cart: {
+            items: [],
+          },
+        })
+        user.save()
+      }
+    })
+    app.listen(
+      3000,
+      console.log(`server is running in port ${process.env.PORT}`)
+    )
+  })
+  .catch((err) => {
+    console.log(err)
+  })
