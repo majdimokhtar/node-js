380646559cfb132e6295d7262adae08c5e070bdb
unknown <majdi.mokhtar@gmail.com>
2022-12-10 10:57:45 +0100

    flash message user experience on login and sign up

--- Authentication/controllers/admin.js
+++ Authentication/controllers/admin.js
@@ -1,107 +1,104 @@
-const Product = require('../models/product');
+const Product = require("../models/product")
 
 exports.getAddProduct = (req, res, next) => {
-  if(!req.session.isLoggedIn){
-  return res.redirect("/login")
+  if (!req.session.isLoggedIn) {
+    return res.redirect("/login")
   }
-  res.render('admin/edit-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
+  res.render("admin/edit-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
     editing: false,
-    isAuthenticated: req.session.isLoggedIn
-  });
-};
+  })
+}
 
 exports.postAddProduct = (req, res, next) => {
-  const title = req.body.title;
-  const imageUrl = req.body.imageUrl;
-  const price = req.body.price;
-  const description = req.body.description;
+  const title = req.body.title
+  const imageUrl = req.body.imageUrl
+  const price = req.body.price
+  const description = req.body.description
   const product = new Product({
     title: title,
     price: price,
     description: description,
     imageUrl: imageUrl,
-    userId: req.user
-  });
+    userId: req.user,
+  })
   product
     .save()
-    .then(result => {
+    .then((result) => {
       // console.log(result);
-      console.log('Created Product');
-      res.redirect('/admin/products');
+      console.log("Created Product")
+      res.redirect("/admin/products")
     })
-    .catch(err => {
-      console.log(err);
-    });
-};
+    .catch((err) => {
+      console.log(err)
+    })
+}
 
 exports.getEditProduct = (req, res, next) => {
-  const editMode = req.query.edit;
+  const editMode = req.query.edit
   if (!editMode) {
-    return res.redirect('/');
+    return res.redirect("/")
   }
-  const prodId = req.params.productId;
+  const prodId = req.params.productId
   Product.findById(prodId)
-    .then(product => {
+    .then((product) => {
       if (!product) {
-        return res.redirect('/');
+        return res.redirect("/")
       }
-      res.render('admin/edit-product', {
-        pageTitle: 'Edit Product',
-        path: '/admin/edit-product',
+      res.render("admin/edit-product", {
+        pageTitle: "Edit Product",
+        path: "/admin/edit-product",
         editing: editMode,
         product: product,
-        isAuthenticated: req.session.isLoggedIn
-      });
+      })
     })
-    .catch(err => console.log(err));
-};
+    .catch((err) => console.log(err))
+}
 
 exports.postEditProduct = (req, res, next) => {
-  const prodId = req.body.productId;
-  const updatedTitle = req.body.title;
-  const updatedPrice = req.body.price;
-  const updatedImageUrl = req.body.imageUrl;
-  const updatedDesc = req.body.description;
+  const prodId = req.body.productId
+  const updatedTitle = req.body.title
+  const updatedPrice = req.body.price
+  const updatedImageUrl = req.body.imageUrl
+  const updatedDesc = req.body.description
 
   Product.findById(prodId)
-    .then(product => {
-      product.title = updatedTitle;
-      product.price = updatedPrice;
-      product.description = updatedDesc;
-      product.imageUrl = updatedImageUrl;
-      return product.save();
+    .then((product) => {
+      product.title = updatedTitle
+      product.price = updatedPrice
+      product.description = updatedDesc
+      product.imageUrl = updatedImageUrl
+      return product.save()
     })
-    .then(result => {
-      console.log('UPDATED PRODUCT!');
-      res.redirect('/admin/products');
+    .then((result) => {
+      console.log("UPDATED PRODUCT!")
+      res.redirect("/admin/products")
     })
-    .catch(err => console.log(err));
-};
+    .catch((err) => console.log(err))
+}
 
 exports.getProducts = (req, res, next) => {
   Product.find()
     // .select('title price -_id')
     // .populate('userId', 'name')
-    .then(products => {
-      console.log(products);
-      res.render('admin/products', {
+    .then((products) => {
+      console.log(products)
+      res.render("admin/products", {
         prods: products,
-        pageTitle: 'Admin Products',
-        path: '/admin/products',
-        isAuthenticated: req.session.isLoggedIn
-      });
+        pageTitle: "Admin Products",
+        path: "/admin/products",
+      })
     })
-    .catch(err => console.log(err));
-};
+    .catch((err) => console.log(err))
+}
 
 exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+  const prodId = req.body.productId
   Product.findByIdAndRemove(prodId)
     .then(() => {
-      console.log('DESTROYED PRODUCT');
-      res.redirect('/admin/products');
+      console.log("DESTROYED PRODUCT")
+      res.redirect("/admin/products")
     })
-    .catch(err => console.log(err));
-};
+    .catch((err) => console.log(err))
+}
--- Authentication/controllers/auth.js
+++ Authentication/controllers/auth.js
@@ -2,18 +2,30 @@ const User = require("../models/user")
 const bcrypt = require("bcryptjs")
 
 exports.getLogin = (req, res, next) => {
+  let message = req.flash("error")
+  if (message.length > 0) {
+    message = message[0]
+  } else {
+    message = null
+  }
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: false,
+    errorMessage: message
   })
 }
 
 exports.getSignup = (req, res, next) => {
+  let message = req.flash("error")
+  if (message.length > 0) {
+    message = message[0]
+  } else {
+    message = null
+  }
   res.render("auth/signup", {
     path: "/signup",
     pageTitle: "Signup",
-    isAuthenticated: false,
+    errorMessage: message
   })
 }
 
@@ -23,6 +35,7 @@ exports.postLogin = (req, res, next) => {
   User.findOne({ email: email })
     .then((user) => {
       if (!user) {
+        req.flash("error", "invalid email or password")
         return res.redirect("/login")
       }
       bcrypt
@@ -36,6 +49,7 @@ exports.postLogin = (req, res, next) => {
               res.redirect("/")
             })
           }
+          req.flash("error", "invalid email or password")
           res.redirect("/login")
         })
         .catch((err) => {
@@ -53,6 +67,7 @@ exports.postSignup = (req, res, next) => {
   User.findOne({ email: email })
     .then((userDoc) => {
       if (userDoc) {
+        req.flash("error", "email exist already")
         return res.redirect("/signup")
       }
       return bcrypt
