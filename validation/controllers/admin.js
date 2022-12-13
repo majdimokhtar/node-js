7c705ca401e4ecdfa8596ba90e82ca4d0750b5a6
Majdi Mokhtar <majdi.mokhtar@gmail.com>
2022-12-13 23:13:28 +0100

    error handling

--- validation/controllers/admin.js
+++ validation/controllers/admin.js
@@ -24,7 +24,7 @@ exports.postAddProduct = (req, res, next) => {
   if (!errors.isEmpty()) {
     return res.status(422).render("admin/edit-product", {
       pageTitle: "Add Product",
-      path: "/admin/edit-product",
+      path: "/admin/add-product",
       editing: false,
       hasError: true,
       product: {
@@ -52,7 +52,21 @@ exports.postAddProduct = (req, res, next) => {
       res.redirect("/admin/products")
     })
     .catch((err) => {
-      console.log(err)
+      res.redirect("/500")
+      // return res.status(500).render("admin/edit-product", {
+      //   pageTitle: "Add Product",
+      //   path: "/admin/add-product",
+      //   editing: false,
+      //   hasError: true,
+      //   product: {
+      //     title: title,
+      //     imageUrl: imageUrl,
+      //     price: price,
+      //     description: description,
+      //   },
+      //   errorMessage: "Database operation failed !!",
+      //   validationErrors:[],
+      // })
     })
 }
 
